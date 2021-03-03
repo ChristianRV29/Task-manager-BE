86711a76a79d722e18d082025d6e0440eb5e3c0f
ChristianRV29 <kmylorv@gmail.com>
2021-03-03 14:13:23 -0500

    Class app.js defined

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to Task-Manager-BE');
+})
+
+app.listen(port, () => {
+    console.log(`The server is running on port: ${port}`);
+});
 
 /**
  * 
