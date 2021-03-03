6cbb5e4a65c57661f54724696317bcbff997d324
ChristianRV29 <kmylorv@gmail.com>
2021-03-03 14:26:20 -0500

    Cors added

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
 const port = process.env.PORT || 8080;
 const app = express();
+
+const env = process.env.ENV || 'Unknown';
+const allowLocalhost = ['dev', 'Unknown'].includes(env);
+
+const corsOption = {
+    origin: function (origin, callback) {
+        const validateOrigin = (origin) || ' ';
+        if ((!origin || allowLocalhost)) {
+            callback(null, true);
+        } else {
+            console.log(`Attemped to provide a response to origin: ${origin}`);
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}
 
 app.get('/', (req, res) => {
     res.status(200).send('Welcome to Task-Manager-BE');
@@ -15,19 +29,5 @@ app.listen(port, () => {
     console.log(`The server is running on port: ${port}`);
 });
 
-/**
- * 
-const corsOptions = {
-  origin: (origin, callback) => {
-    log.info(`origin: ${origin}`);
-    const validateOrigin = (origin) || ' ';
-    if ((validateOrigin.match(regex)) || (!origin || allowLocalhost)) {
-      callback(null, true);
-    } else {
-      log.warn(`Attempted to provide a response to origin: ${origin}`);
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
- */
+
 
