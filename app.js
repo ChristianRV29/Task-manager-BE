86434787e7400cfec6072d80de2b44a068166771
ChristianRV29 <kmylorv@gmail.com>
2021-03-03 14:01:58 -0500

    eslintignore added

--- app.js
+++ app.js
@@ -3,4 +3,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const port = process.env.PORT || 8080;
+const app = express();
+
+
+/**
+ * 
+const corsOptions = {
+  origin: (origin, callback) => {
+    log.info(`origin: ${origin}`);
+    const validateOrigin = (origin) || ' ';
+    if ((validateOrigin.match(regex)) || (!origin || allowLocalhost)) {
+      callback(null, true);
+    } else {
+      log.warn(`Attempted to provide a response to origin: ${origin}`);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+ */
 
