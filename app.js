0abe63ea6327db39c3866740b5ecd705835a088c
ChristianRV29 <kmylorv@gmail.com>
2021-03-04 10:24:06 -0500

    Eslint implemented

--- app.js
+++ app.js
@@ -12,24 +12,23 @@ const env = process.env.ENV || 'Unknown';
 const allowLocalhost = ['dev', 'Unknown'].includes(env);
 
 const corsOption = {
-    origin: function (origin, callback) {
-        const validateOrigin = (origin) || ' ';
-        if ((!origin || allowLocalhost)) {
-            callback(null, true);
-        } else {
-            console.log(`Attemped to provide a response to origin: ${origin}`);
-            callback(new Error('Not allowed by CORS'));
-        }
+  origin(origin, callback) {
+    const validateOrigin = (origin) || ' ';
+    if ((!origin || allowLocalhost)) {
+      callback(null, true);
+    } else {
+      console.log(`Attemped to provide a response to origin: ${origin}`);
+      callback(new Error('Not allowed by CORS'));
     }
-}
+  },
+};
 
-app.use(allowLocalhost ? cors(): cors(corsOption));
+app.use(allowLocalhost ? cors() : cors(corsOption));
 app.use(bodyParser.json({ limit: '600mb' }));
 app.use(bodyParser.urlencoded({ limit: '600mb', extended: true }));
 
 app.use('/api/', statusRoutes);
 
-
 app.listen(port, () => {
-    console.log(`The server is running on port: ${port}`);
+  console.log(`The server is running on port: ${port}`);
 });
---
+++ utils/logger.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+const appName = process.env.APP_NAME || 'Task-manager';
+const env = process.env.ENV || 'Unknown';
+
+const { log } = console;
+
+
